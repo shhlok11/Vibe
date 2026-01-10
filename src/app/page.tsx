76c5feebb9aa585c1095096f55bcc85a5e3e438c
shhlok11 <yashlok8425@gmail.com>
2026-01-10 02:14:07 -0500

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { useMutation } from "@tanstack/react-query";
-import React, { Suspense, useState } from "react";
+import React, { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
@@ -14,11 +14,14 @@ const Page = () => {
       onSuccess: () => {
         toast.success("Background Job started");
       },
+      onError: (error) => {
+        toast.error(`Failed to start job: ${error.message}`);
+      },
     })
   );
   const [value, setValue] = useState("");
   return (
-    <div className="p-4 mx-w-7xl mx-auto">
+    <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
